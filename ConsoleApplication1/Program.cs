c1d881e81a687b6ce5fb9ddfff274576e5baa14c
Arkadiy Parinov <arkadiy.parinov@yandex.ru>
2016-06-14 15:15:54 +1000

    huffman algorithm is completed
    coding and decoding

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
@@ -11,45 +10,102 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             var input = Console.ReadLine();
-           
-            var nodesList = input
-                            .ToCharArray()
-                            .Distinct()
-                            .Select(t => new Node()
-                            {
-                                Letter = t,
-                                Frequency = input.Count(x => x == t),
-                                IsLetter = true
-                            })
-                            .OrderBy(t => t.Frequency)
-                            .ToList();
 
-            var nodes = new PQueue<Node>(nodesList);
-
-            var count = nodes.Count;
-            for (int i = 0; i < count - 1; i++)
+            if (input != null)
             {
-                var left = nodes.Dequeue();
-                var right = nodes.Dequeue();
-               
-                nodes.Enqueue(new Node()
+                var nodesList = input
+                    .ToCharArray()
+                    .Distinct()
+                    .Select(t => new Node()
+                    {
+                        Letter = t,
+                        Frequency = input.Count(x => x == t),
+                        IsLetter = true
+                    })
+                    .OrderBy(t => t.Frequency)
+                    .ToList();
+                var codes = new Dictionary<char, string>();
+            
+                if (nodesList.Count == 1)
                 {
-                    Frequency = left.Frequency + right.Frequency,
-                    Left = left,
-                    Right = right,
-                    IsLetter = false
-                });
+                    codes.Add(nodesList.First().Letter, "0");
+                }
+                else
+                {
+
+                    var nodes = new PQueue<Node>(nodesList);
+
+                    var count = nodes.Count;
+                    for (int i = 0; i < count - 1; i++)
+                    {
+                        var left = nodes.Dequeue();
+                        var right = nodes.Dequeue();
+
+                        nodes.Enqueue(new Node()
+                        {
+                            Frequency = left.Frequency + right.Frequency,
+                            Left = left,
+                            Right = right,
+                            IsLetter = false
+                        });
+                    }
+
+                    var tree = nodes.Dequeue();
+                    codes = GetDictionaryFromTree(tree);
+                }
+               
+                var n = codes.Count;
+                var output = GetOutput(codes, input);
+
+                Console.WriteLine(n + " " + output.Length);
+                foreach (var c in codes)
+                {
+                    Console.WriteLine($"{c.Key}: {c.Value}");
+                }
+                Console.WriteLine(output);
             }
+        }
+
+        public static string GetOutput(Dictionary<char, string> codes, string input)
+        {
+            var sb = new StringBuilder();
+            foreach (var c in input.ToCharArray())
+            {
+                sb.Append(codes[c]);
+            }
+
+            return sb.ToString();
+        }
+
+        public static Dictionary<char, string> GetDictionaryFromTree(Node tree)
+        {
+            var stack = new Stack<NodeInfo>();
+            var result = new Dictionary<char, string>();
+            stack.Push(new NodeInfo(tree) {Code = new List<bool>()});
+            while (stack.Count != 0)
+            {
+                var item = stack.Pop();
+                if (!item.Node.IfLeaf)
+                {
+                    stack.Push(new NodeInfo(item.Node.Right) {Code = new List<bool>(item.Code) {true}});
+
+                    stack.Push(new NodeInfo(item.Node.Left) {Code = new List<bool>(item.Code) {false}});
+                }
+                else
+                {
+                    var codeString = item.Code.Aggregate("", (current, c) => current + Convert.ToInt32(c).ToString());
+                    result.Add(item.Node.Letter,codeString);
+                }
+            }
+
+            return result;
         }
 
         public class PQueue<T> where T : IComparable
         {
             private PQueueItem<T> _startItem;
             private int _count = 0;
-            public int Count
-            {
-                get { return _count; }
-            }
+            public int Count => _count;
 
             public PQueue()
             {
@@ -72,6 +128,7 @@ namespace ConsoleApplication1
             {
                 _count++;
                 var stepItem = _startItem;
+                var temp = _startItem;
                 var newItem = new PQueueItem<T>() {Item = item};
                 if (_startItem == null)
                 {
@@ -79,7 +136,7 @@ namespace ConsoleApplication1
                     return;
                 }
 
-                while (stepItem.Child != null)
+                while (stepItem != null)
                 {
                     if (item.CompareTo(stepItem.Item) <=0)
                     {
@@ -99,12 +156,13 @@ namespace ConsoleApplication1
                         }
                         return;
                     }
+                    temp = stepItem;
                     stepItem = stepItem.Child;
                 }
 
                 //last element
-                newItem.Parent = stepItem;
-                stepItem.Child = newItem;
+                newItem.Parent = temp;
+                temp.Child = newItem;
             }
 
             public T Dequeue()
@@ -114,7 +172,7 @@ namespace ConsoleApplication1
                     _count--;
                     var result = _startItem.Item;
                     _startItem = _startItem.Child;
-                    _startItem.Parent = null;
+                    if (_startItem != null) _startItem.Parent = null;
                     return result;
                 }
                 return default(T);
@@ -140,6 +198,17 @@ namespace ConsoleApplication1
             public PQueueItem<T> Parent { get; set; }
             public PQueueItem<T> Child { get; set; }
             public T Item { get; set; }
+        }
+
+        public class NodeInfo
+        {
+            public Node Node { get; set; }
+            public List<bool> Code { get; set; }
+
+            public NodeInfo(Node node)
+            {
+                Node = node;
+            }
         }
 
         public class Node : IComparable
---
+++ app3.2.2/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace app3._2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split();
+            var n = int.Parse(input[0]);
+            var l = int.Parse(input[1]);
+
+            var codes = new Dictionary<char, string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                input = Console.ReadLine().Split(new string[]{": "}, StringSplitOptions.None);
+                codes.Add(input[0][0], input[1]);
+            }
+            var code = Console.ReadLine();
+
+            Console.WriteLine(GetTextByCode(code, codes));
+        }
+
+        public static string GetTextByCode(string code, Dictionary<char, string> codes)
+        {
+            var value = "";
+            var result = "";
+            for (int i = 0; i < code.Length; i++)
+            {
+                value += code[i];
+                if (codes.ContainsValue(value))
+                {
+                    result += codes.FirstOrDefault(t => t.Value == value).Key;
+                    value = "";
+                }
+            }
+
+            return result;
+        }
+    }
+}
