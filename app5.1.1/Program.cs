9f17e7e5d10c78048aa18dbec4b081406e78469f
Arkadiy Parinov <arkadiy.parinov@yandex.ru>
2016-06-16 12:19:22 +1000

    add simple binary search implementation

---
+++ app5.1.1/Program.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+using System.Linq;
+
+namespace app5._1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input1 = Console.ReadLine();
+            input1 = input1.Substring(input1.IndexOf(" "));
+            var array = input1.Split()
+                                    .Where(t => t != "")
+                                    .Select(t => int.Parse(t))
+                                    .ToArray();
+
+            var input2 = Console.ReadLine();
+            input2 = input2.Substring(input2.IndexOf(" "));
+            var items = input2.Split()
+                                    .Where(t => t != "")
+                                    .Select(t => int.Parse(t))
+                                    .ToArray();
+            foreach (var item in items)
+            {
+                var result = BinarySearch(array, item);
+                if (result != -1) result++;
+                Console.Write("{0} ", result);
+            }
+        }
+
+        public static int BinarySearch(int[] array, int item)
+        {
+            int l = 0, r = array.Length - 1, m = 0;
+
+            while (l <= r)
+            {
+                m = (l + r)/2;
+                if (array[m] == item) return m;
+                if (array[m] > item) r = m - 1;
+                else l = m + 1;
+            }
+            return -1;
+        }
+    }
+}
