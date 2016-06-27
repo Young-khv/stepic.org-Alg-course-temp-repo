652c97e098bc07a48eb558c429aacd3a225a29c4
Arkadiy Parinov <arkadiy.parinov@yandex.ru>
2016-06-27 21:27:49 +1000

    create new project for longest non increasing subsequence with nlogn complexity

---
+++ app8.2.1/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Linq;
+
+namespace app8._2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            var array = Console.ReadLine()
+                                        .Split()
+                                        .Select(int.Parse)
+                                        .ToArray();
+
+            Console.WriteLine(GetSequenceQty(array));
+        }
+
+        private static int GetSequenceQty(int[] array)
+        {
+            var path = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                path[i] = 1;
+
+                for (int j = 0; j < i; j++)
+                {
+                    if (array[i]%array[j] == 0 && path[j] + 1 > path[i])
+                        path[i] = path[j] + 1;
+                }
+            }
+
+            return path.Max();
+        }
+    }
+}
---
+++ app8.2.2/Program.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace app8._2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            var array = Console.ReadLine()
+                                        .Split()
+                                        .Select(int.Parse)
+                                        .ToArray();
+
+            var result = NISequence(array);
+            Console.WriteLine(result.Count);
+            for (int i = result.Count - 1; i > -1; i--)
+            {
+                Console.Write("{0} ", result[i]);
+            }
+        }
+
+        public static List<int> NISequence(int[] array)
+        {
+            var result = new List<int>() {array[0]};
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > result[0])
+                {
+                    result[0] = array[i];
+                }
+                else if (array[i] <= result[result.Count - 1])
+                {
+                    result.Add(array[i]);
+                }
+                else
+                {
+
+                }
+            }
+
+            return result;
+        }
+    }
+}
