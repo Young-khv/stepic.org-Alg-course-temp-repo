dd450551be17715ac2661095bbdd6942c146b301
Arkadiy Parinov <arkadiy.parinov@yandex.ru>
2016-07-10 18:07:21 +1000

    Fix some mistakes
    Wrong answer for test: 9, 10, 6, 3, 6, 8, 7, 9, 6, 5

--- app8.2.2/Program.cs
+++ app8.2.2/Program.cs
@@ -1,8 +1,9 @@
 ﻿
 using System;
-using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Text;
 
 namespace app8._2._2
 {
@@ -20,7 +21,7 @@ namespace app8._2._2
             Console.WriteLine(result.Count);
             for (int i = result.Count - 1; i > -1; i--)
             {
-                Console.Write("{0} ", result[i]);
+                Console.Write("{0} ", result[i] + 1);
             }
         }
 
@@ -44,8 +45,8 @@ namespace app8._2._2
                 }
                 else if (array[i] <= array[t[len]])
                 {
-                    r[len + 1] = i;
-                    t[i] = len++;
+                    r[i] = t[len++];
+                    t[len] = i;
                 }
                 else if (array[i] > array[t[0]])
                 {
@@ -53,22 +54,66 @@ namespace app8._2._2
                 }
                 else
                 {
-                    SetItemToRightPosition(array, i, r, t, len);
+                   SetItemToRightPosition(array, i, r, t, len);
                 }
 
             }
-            return null;
+
+            var result = new List<int>();
+            var startIdx = -1;
+            var max = -1;
+            for (int i = 0; i < r.Length; i++)
+            {
+                if (r[i] > max)
+                {
+                    max = r[i];
+                    startIdx = i;
+                }
+            }
+
+            if (startIdx == -1)
+            {
+                result.Add(0);
+                return result;
+            }
+
+            while (startIdx != -1)
+            {
+                result.Add(startIdx);
+                startIdx = r[startIdx];
+            }
+           
+            return result;
         }
 
-        public static void SetItemToRightPosition(int[]array, int index, int[] r, int[] t, int len)
+        public static void SetItemToRightPosition(int[] array, int index, int[] r, int[] t, int len)
         {
-            var idx = GetIndex(array, r, array[index]);
+            var idx = GetIndex(array, t, array[index], len);
+            if (idx == -1)
+            {
+                var message = new StringBuilder();
+                message.Append($"Can't find valuable index for item: {array[index]} in array: {array[t[0]]}");
+                for (int i = 1; i < t.Length; i++)
+                {
+                    if(t[i] == 0) break;
+                    message.Append($", {array[t[i]]}");
+                }
+
+                throw new Exception(message.ToString());
+            }
+          
+            t[idx] = index;
+            r[index] = idx - 1;// TODO: look to this code part
+
         }
 
-        public static int GetIndex(int[] array,int[]idxs, int item)
+        public static int GetIndex(int[] array, int[]idxs, int item, int len)
         {
-            var l = 1;
-            var r = array.Length - 1;
+            if (len + 1 == 2)
+                return len;
+
+            var l = 0;
+            var r = len;
             while (l + 1 != r)
             {
                 int i = (l + r) / 2;
@@ -81,8 +126,8 @@ namespace app8._2._2
                     l = i;
                 }
             }
-            if (array[idxs[l]] == item && array[idxs[r]] < item)
-                return r + 1;
+            if (array[idxs[l]] >= item && array[idxs[r]] < item)
+                return r;
 
             return -1;
         }
