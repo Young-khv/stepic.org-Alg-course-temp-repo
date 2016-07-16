de8e4455bcdce58068c55aed1c739b1d26f869be
Arkadiy Parinov <arkadiy.parinov@yandex.ru>
2016-07-16 13:27:30 +1000

    add 2 solutions for knapsack problem and counting of Levenshtein distance

---
+++ app8.3.1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace app8._3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s1 = Console.ReadLine();
+            var s2 = Console.ReadLine();
+
+            Console.WriteLine(LevenshteinDistance(s1,s2));
+        }
+
+        public static int LevenshteinDistance(string first, string second)
+        {
+            var opt = new int[first.Length + 1, second.Length + 1];
+            for (var i = 0; i <= first.Length; ++i) opt[i, 0] = i;
+            for (var i = 0; i <= second.Length; ++i) opt[0, i] = i;
+            for (var i = 1; i <= first.Length; ++i)
+                for (var j = 1; j <= second.Length; ++j)
+                {
+                    if (first[i - 1] == second[j - 1])
+                        opt[i, j] = opt[i - 1, j - 1];
+                    else
+                        opt[i, j] = MinOfThree(opt[i - 1, j], opt[i - 1, j - 1], opt[i, j - 1]) + 1;
+                }
+            return opt[first.Length, second.Length];
+        }
+
+        public static int MinOfThree(int a, int b, int c)
+        {
+            return Math.Min(Math.Min(a, b), c);
+        }
+    }
+}
---
+++ app8.4.1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app8._4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split()
+                                          .Select(int.Parse)
+                                          .ToArray();
+            var w = input[0];
+            var n = input[1];
+
+            var items = Console.ReadLine().Split()
+                                          .Select(int.Parse);
+
+            var itemsList = new List<int>() {0};
+            itemsList.AddRange(items);
+            Console.WriteLine(Knapsack(w, itemsList));
+
+        }
+
+        public static int Knapsack(int W, List<int> items)
+        {
+            var D = new int[W + 1, items.Count+1];
+            for (int i = 0; i <= W; i++) D[i, 0] = 0;
+            for (int i = 0; i <= items.Count; i++) D[0, i] = 0;
+
+            for (int i=1;i<items.Count;i++)
+                for (int w = 1; w <= W; w++)
+                {
+                    D[w, i] = D[w, i - 1];
+                    if (items[i] <= w)
+                    {
+                        D[w, i] = Math.Max(D[w, i], D[w - items[i],i-1] + items[i]);
+                    }
+                }
+
+            return D[W, items.Count-1];
+        }
+    }
+}
